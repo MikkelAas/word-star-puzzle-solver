74bbd0c1e01b7d07be1ee619a360336b49cc757b
Mikkel Aas <aa.mikkel2902@gmail.com>
2024-01-27 15:08:10 +0100

    map words to uppercase

--- src/puzzle_solver.rs
+++ src/puzzle_solver.rs
@@ -12,7 +12,7 @@ fn read_file_to_lines() -> Vec<String> {
     include_str!("words.csv")
         .to_owned()
         .lines()
-        .map(String::from)
+        .map(|line| String::from(line.to_ascii_uppercase()))
         .collect()
 }
 
@@ -20,16 +20,10 @@ fn filter_puzzle_input(lines: Vec<String>, puzzle_input: PuzzleInput) -> HashSet
     let filtered_lines: Vec<String> = lines
         .into_iter()
         .filter(|line| line.len() > 3)
+        .filter(|line| line.contains(puzzle_input.required_character))
         .filter(|line| {
-            line.to_ascii_uppercase()
-                .contains(puzzle_input.required_character)
-        })
-        .filter(|line| {
-            line.chars().all(|char| {
-                puzzle_input
-                    .allowed_characters
-                    .contains(&char.to_ascii_uppercase())
-            })
+            line.chars()
+                .all(|char| puzzle_input.allowed_characters.contains(&char))
         })
         .collect();
 
@@ -43,10 +37,10 @@ mod tests {
     #[test]
     fn test_filter_puzzle_input() {
         let words = vec![
-            String::from("Tennis"),
-            String::from("Videospill"),
-            String::from("Tur"),
-            String::from("Kaffe"),
+            String::from("TENNIS"),
+            String::from("VIDEOSPILL"),
+            String::from("TUR"),
+            String::from("KAFFE"),
         ];
 
         let puzzle_input = PuzzleInput {
@@ -56,7 +50,7 @@ mod tests {
 
         let result = filter_puzzle_input(words, puzzle_input);
 
-        let expected = HashSet::from([String::from("Tur")]);
+        let expected = HashSet::from([String::from("TUR")]);
 
         assert_eq!(expected, result)
     }
